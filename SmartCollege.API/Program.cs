dc302ee9ea8f556f475885ae0e7a59a9f54fda2a
little-junior <rampagedolol@outlook.com>
2024-02-27 22:20:00 -0300

    feat: add DbContext service

--- SmartCollege.API/Program.cs
+++ SmartCollege.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SmartCollege.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<SmartCollegeContext>(context =>
+{
+    context.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
